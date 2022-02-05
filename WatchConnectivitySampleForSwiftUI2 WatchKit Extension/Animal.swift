34ec72fd94014ee4432c1c57f5914019a1fe4acb
Takuya Aso <milanista224@gmail.com>
2022-02-05 23:13:35 +0900

    Add Animal struct.

---
+++ WatchConnectivitySampleForSwiftUI2 WatchKit Extension/Animal.swift
@@ -0,0 +1,13 @@
+//
+//  Animal.swift
+//  WatchConnectivitySampleForSwiftUI2
+//
+//  Created by Takuya Aso on 2022/02/05.
+//
+
+import Foundation
+
+struct Animal: Codable, Hashable {
+    var name: String
+    var emoji: String
+}
